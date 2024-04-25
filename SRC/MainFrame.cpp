319fa1b71fa9d643ab131e8f4b8aa4ca45e8b4d4
Andrzej <mr.andi321@gmail.com>
2024-04-25 20:58:49 +0200

    Added starting template

---
+++ SRC/MainFrame.cpp
@@ -0,0 +1,45 @@
+//
+// Created by andrzej on 22.03.24.
+//
+#include <wx/wxprec.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnExit(wxCommandEvent& event);
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    SetProcessDPIAware();
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "wxStart")
+{
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(wxID_EXIT);
+
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+
+    SetMenuBar(menuBar);
+
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
